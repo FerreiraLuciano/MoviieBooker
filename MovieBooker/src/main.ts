af361772c2d4614cd1bec42ffc93c9a4ad667b92
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2025-02-07 22:19:16 +0100

    added missing bearer token to swagger

--- MovieBooker/src/main.ts
+++ MovieBooker/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setTitle('MoviieBroker')
     .setDescription('The MoviieBroker API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
